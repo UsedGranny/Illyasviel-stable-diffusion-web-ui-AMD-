221495099c3dab5a7f6adf58ea9c1876041bef9d
UsedGranny <kuba.mosler@gmail.com>
2026-07-09 21:33:03 +0200

    Modify webui-user.bat for environment setup
    Updated environment variables and installation commands for improved compatibility and dependency management.

--- webui-user.bat
+++ webui-user.bat
@@ -3,16 +3,16 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --use-directml --no-half-vae --disable-nan-check
 
-set HSA_OVERRIDE_GFX_VERSION=10.3.0
+:: Force RDNA 4 AI Hardware Acceleration
+set HSA_OVERRIDE_GFX_VERSION=11.0.2
 set HIP_VISIBLE_DEVICES=0
-set TORCH_COMMAND=pip install torch==2.3.1+cpu torchvision==0.18.1+cpu --index-url pytorch.org
 
-set GIT_PYTHON_REFRESH=quiet
-set GIT_PYTHON_GIT_EXECUTABLE=%~dp0..\system\git\bin\git.exe
+:: CPU-Base Dependency Trick to bypass Nvidia check
+set TORCH_COMMAND=pip install torch==2.3.1+cpu torchvision==0.18.1+cpu --index-url https://pytorch.org
 
-venv\Scripts\python.exe -m pip install fastapi psutil safetensors gradio transformers timm einops pytorch_lightning omegaconf diskcache scikit-image clean-fid GitPython piexif blendmodes opencv-python lark torchdiffeq k-diffusion accelerate fonts font-roboto piq open-clip-torch setuptools
-
-set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half --disable-nan-check
+:: Force-feeding dependencies into venv pipeline
+set PRE_LAUNCH_COMMAND=".\venv\Scripts\python.exe" -m pip install gradio transformers accelerate safetensors
 
 call webui.bat
